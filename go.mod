f5a9517f8dd8cbb933f0be408e51589cf7cd1551
Peter Preeper <ppreeper@gmail.com>
2026-04-16 09:57:39 -0600

    refactor rd to single-pass prune

--- go.mod
+++ go.mod
@@ -2,28 +2,4 @@ module github.com/ppreeper/rd
 
 go 1.23.1
 
-require (
-	github.com/jmoiron/sqlx v1.4.0
-	github.com/schollz/progressbar/v3 v3.18.0
-	github.com/urfave/cli/v2 v2.27.6
-	modernc.org/sqlite v1.37.1
-)
-
-require (
-	github.com/cpuguy83/go-md2man/v2 v2.0.7 // indirect
-	github.com/dustin/go-humanize v1.0.1 // indirect
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
-	github.com/ncruces/go-strftime v0.1.9 // indirect
-	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
-	github.com/rivo/uniseg v0.4.7 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
-	golang.org/x/exp v0.0.0-20250506013437-ce4c2cf36ca6 // indirect
-	golang.org/x/sys v0.33.0 // indirect
-	golang.org/x/term v0.32.0 // indirect
-	modernc.org/libc v1.65.7 // indirect
-	modernc.org/mathutil v1.7.1 // indirect
-	modernc.org/memory v1.11.0 // indirect
-)
+require github.com/urfave/cli/v3 v3.8.0
--- rd.go
+++ rd.go
@@ -1,176 +1,149 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
+	"sync/atomic"
+	"time"
 
-	// _ "github.com/glebarez/go-sqlite"
-	"github.com/jmoiron/sqlx"
-	"github.com/schollz/progressbar/v3"
-	"github.com/urfave/cli/v2"
-	_ "modernc.org/sqlite"
+	"github.com/urfave/cli/v3"
 )
 
 func main() {
-	// Initialize Application
-	a := DAPP{}
-	fdb := ":memory:"
-	// fdb := ".filehashes.db"
-	var err error
-
-	os.Remove(fdb)
-	a.DB, err = sqlx.Open("sqlite", fdb)
-	if err != nil {
-		fmt.Printf("error: %v\n", err)
-		return
-	}
-	defer a.DB.Close()
-	defer os.Remove(fdb)
-
-	// Initialize Database
-	if err := a.InitializeDatabase(); err != nil {
-		fmt.Printf("database initialization failed: %v\n", err)
-		return
-	}
-
-	var verboseCount int
-	app := &cli.App{
+	app := &cli.Command{
 		Name:  "rd",
 		Usage: "remove empty directories",
 		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:    "dry-run",
+				Aliases: []string{"n"},
+				Usage:   "count directories that would be removed",
+			},
 			&cli.BoolFlag{
 				Name:    "verbose",
 				Aliases: []string{"v"},
 				Usage:   "increase verbosity",
 				Value:   false,
-				Count:   &verboseCount,
 			},
 		},
-		Action: func(cCtx *cli.Context) error {
-			a.Dirs = cCtx.Args().Slice()
-			if len(a.Dirs) == 0 {
-				a.Dirs = append(a.Dirs, ".")
-			}
-			a.Verbose = cCtx.Bool("verbose")
-
-			// fmt.Println("Filewalker")
-			if err := a.Filewalker(); err != nil {
-				fmt.Printf("filewalker error: %v\n", err)
-				return err
+		Action: func(_ context.Context, cCtx *cli.Command) error {
+			dirs := cCtx.Args().Slice()
+			if len(dirs) == 0 {
+				dirs = []string{"."}
 			}
 
-			err = a.TrimDirs()
+			progress := newProgressReporter(cCtx.Bool("dry-run"))
+			defer progress.Stop()
+
+			removed, err := pruneEmptyDirs(dirs, cCtx.Bool("dry-run"), progress)
 			if err != nil {
-				fmt.Printf("trim dirs error: %v\n", err)
 				return err
 			}
-
+			if cCtx.Bool("dry-run") {
+				fmt.Printf("would remove %d directories\n", removed)
+			} else if cCtx.Bool("verbose") {
+				fmt.Printf("removed %d directories\n", removed)
+			}
 			return nil
 		},
 	}
-	if err := app.Run(os.Args); err != nil {
+	if err := app.Run(context.Background(), os.Args); err != nil {
 		fmt.Printf("error running app: %v\n", err)
 	}
 }
 
-type DAPP struct {
-	Dirs    []string
-	Verbose bool
-	DB      *sqlx.DB
-}
-
-func (a *DAPP) Filewalker() error {
-	for _, dir := range a.Dirs {
-		bar := progressbar.NewOptions(-1,
-			progressbar.OptionSetWriter(os.Stderr),
-			progressbar.OptionClearOnFinish(),
-			progressbar.OptionShowCount(),
-			progressbar.OptionShowIts(),
-			progressbar.OptionSetItsString("files"),
-			progressbar.OptionSetElapsedTime(false),
-			progressbar.OptionSetPredictTime(false),
-			progressbar.OptionSetWidth(15),
-			progressbar.OptionSetDescription("scanning for directories"),
-		)
-		err := filepath.WalkDir(dir, func(path string, d os.DirEntry, err error) error {
-			if err != nil {
-				return fmt.Errorf("error: %w", err)
-			}
-			if d.IsDir() {
-				// store in database
-				slashCount := strings.Count(path, string(os.PathSeparator))
-				err = a.StoreDirInDatabase(path, uint64(slashCount))
-				if err != nil {
-					return fmt.Errorf("error: %w", err)
-				}
-				return nil
-			}
-			bar.Add(1)
-			return nil
-		})
+func pruneEmptyDirs(roots []string, dryRun bool, p *progressReporter) (int, error) {
+	total := 0
+	for _, root := range roots {
+		_, removed, err := pruneDir(root, dryRun, p)
 		if err != nil {
-			return err
+			return total, err
 		}
-		bar.Finish()
+		total += removed
 	}
-	return nil
+	return total, nil
 }
 
-func (a *DAPP) TrimDirs() error {
-	dirList, err := a.GetFilesFromDatabase()
+func pruneDir(path string, dryRun bool, p *progressReporter) (bool, int, error) {
+	p.Scanned()
+	entries, err := os.ReadDir(path)
 	if err != nil {
-		return fmt.Errorf("error retrieving directory list: %w", err)
+		return false, 0, err
 	}
 
-	bar := progressbar.NewOptions(len(dirList),
-		progressbar.OptionSetWriter(os.Stderr),
-		progressbar.OptionClearOnFinish(),
-		progressbar.OptionShowCount(),
-		progressbar.OptionShowIts(),
-		progressbar.OptionSetItsString("directories"),
-		progressbar.OptionSetElapsedTime(false),
-		progressbar.OptionSetPredictTime(false),
-		progressbar.OptionSetWidth(15),
-		progressbar.OptionSetDescription("removing empty directories"),
-	)
-	for _, d := range dirList {
-		if err := os.Remove(d.Path); err != nil {
-			if a.Verbose {
-				fmt.Println("error removing directory:", err)
-			}
+	hasRemaining := false
+	removedCount := 0
+
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			hasRemaining = true
+			continue
 		}
-		bar.Add(1)
+
+		childRemoved, childCount, err := pruneDir(filepath.Join(path, entry.Name()), dryRun, p)
+		if err != nil {
+			return false, removedCount, err
+		}
+		removedCount += childCount
+		if !childRemoved {
+			hasRemaining = true
+		}
 	}
-	bar.Finish()
 
-	return nil
+	if hasRemaining {
+		return false, removedCount, nil
+	}
+
+	if !dryRun {
+		if err := os.Remove(path); err != nil {
+			return false, removedCount, err
+		}
+	}
+	p.Removed()
+
+	return true, removedCount + 1, nil
 }
 
-func (a *DAPP) InitializeDatabase() error {
-	createTableSQL := `CREATE TABLE IF NOT EXISTS dir_list (
-		path TEXT NOT NULL,
-		size UINT64
-		);`
-
-	_, err := a.DB.Exec(createTableSQL)
-	return err
+type progressReporter struct {
+	dryRun  bool
+	scanned atomic.Int64
+	removed atomic.Int64
+	done    chan struct{}
 }
 
-func (a *DAPP) StoreDirInDatabase(path string, size uint64) error {
-	storeFileSQL := `INSERT INTO dir_list (path,size) VALUES (?,?);`
-	_, err := a.DB.Exec(storeFileSQL, path, size)
-	return err
+func newProgressReporter(dryRun bool) *progressReporter {
+	p := &progressReporter{dryRun: dryRun, done: make(chan struct{})}
+	go p.run()
+	return p
 }
 
-type Directories struct {
-	Path string
-}
+func (p *progressReporter) Scanned() { p.scanned.Add(1) }
+func (p *progressReporter) Removed() { p.removed.Add(1) }
+func (p *progressReporter) Stop()    { close(p.done) }
 
-func (a *DAPP) GetFilesFromDatabase() ([]Directories, error) {
-	dirList := []Directories{}
-	SELECT_DIRS := `SELECT path FROM dir_list ORDER BY size desc, path`
-	err := a.DB.Select(&dirList, SELECT_DIRS)
-	return dirList, err
+func (p *progressReporter) run() {
+	ticker := time.NewTicker(250 * time.Millisecond)
+	defer ticker.Stop()
+
+	frames := []string{"|", "/", "-", "\\"}
+	i := 0
+	for {
+		select {
+		case <-ticker.C:
+			fmt.Fprintf(os.Stderr, "\r%s scanned=%d removed=%d", frames[i%len(frames)], p.scanned.Load(), p.removed.Load())
+			if p.dryRun {
+				fmt.Fprint(os.Stderr, " dry-run")
+			}
+			i++
+		case <-p.done:
+			fmt.Fprintf(os.Stderr, "\rscanned=%d removed=%d", p.scanned.Load(), p.removed.Load())
+			if p.dryRun {
+				fmt.Fprint(os.Stderr, " dry-run")
+			}
+			fmt.Fprintln(os.Stderr)
+			return
+		}
+	}
 }
